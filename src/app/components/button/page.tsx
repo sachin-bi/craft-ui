8f188a3102523bf571da557e82da9119c4bdc84f
sachinflux <nandansachitruth@gmail.com>
2026-03-19 01:37:56 +0530

    feat: add button component and utility functions
    - Introduced a new Button component with variants and sizes using class-variance-authority.
    - Added utility function `cn` for merging class names with tailwind-merge.
    - Updated the button page to showcase the new Button component.
    - Enhanced global styles with new color variables and dark mode support.
    - Updated package.json to include new dependencies for UI components and utilities.

--- src/app/components/button/page.tsx
+++ src/app/components/button/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+// import React from 'react'
+import { Button } from "@/components/ui/button";
 
 export default function page() {
   return (
-    <div>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black gap-4">
       button page
+      <Button variant="outline">Button</Button>
+      <Button>Default</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline" size="sm">
+        Button Small
+      </Button>
+
     </div>
   )
 }
