f189bd6e8f31aae96298a3109988a1c5d2a33a18
sachin-bi <sachinandanp5@gmail.com>
2026-03-26 00:31:51 +0530

    feat: add (ProfileDropdown) component and create dropdown page

---
+++ src/app/components/dropdown/page.tsx
@@ -0,0 +1,9 @@
+import { ProfileDropdown } from "@/components/ui/profile-dropdown";
+
+export default function DropdownPage() {
+    return (
+        <div className="min-h-screen bg-slate-50 p-10 dark:bg-gray-900 flex justify-center">
+            <ProfileDropdown />
+        </div>
+    );
+}
